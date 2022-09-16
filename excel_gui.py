f0150f8d50bf0133dcdd27b349425f3a46c76326
German Paul <99292019+GermanPaul12@users.noreply.github.com>
2022-09-16 14:54:21 +0200

    Add files via upload

--- excel_gui.py
+++ excel_gui.py
@@ -1,8 +1,12 @@
 from tkinter import * 
 import customtkinter
-from main import daterange
+from main import *
 import datetime as dt
 import pandas as pd
+from prettytable import PrettyTable, from_csv
+import numpy as np
+
+x = PrettyTable()
 
 root = customtkinter.CTk()
 
@@ -13,19 +17,23 @@ customtkinter.set_default_color_theme("blue")  # Themes: blue (default), dark-bl
 root.geometry("1000x500")
 root.configure(bg='black')
 
-
-dates = [dtt.strftime('%Y-%m-%d') for dtt in daterange(dt.datetime.strptime(f"{dt.datetime.now().year-2}-01-01", "%Y-%m-%d"),
+'''
+dates = [dtt.strftime('%Y-%m-%d') for dtt in daterange(dt.datetime.strptime(f"{dt.datetime.now().year-2}-01-03", "%Y-%m-%d"),
                                                                                              dt.datetime.strptime("2025-09-30", "%Y-%m-%d"))]
 
 kommen = ['08:00' for i in range(len(dates))]
 pause = ['0:30' for i in range(len(dates))]
-gehen = ['16:30' if i % 3 != 0 else '15:30' for i in range(len(dates))]    
+gehen = ['16:30' if i % 7 != 0 else '15:30' for i in range(len(dates))]    
+soll_zeit = ['08:00' if i % 7 != 0 else '07:00' for i in range(len(dates))]  
+ist_zeit = ['08:00' if i % 7 != 0 else '07:00' for i in range(len(dates))]
+zeitkonto = ['00:00' for i in range(len(dates))]
 
 weekday = [dt.datetime.strptime(date, '%Y-%m-%d').weekday() for date in dates]
 dict_wochentag = {0:'Montag', 1:'Dienstag', 2:'Mittwoch', 3:'Donnerstag', 4:'Freitag', 5:'Samstag', 6:'Sonntag'}
 wochentag = [dict_wochentag[i] for i in weekday]
 
-dict_data = {'Datum':dates, 'Kommen':kommen, 'Pause':pause, 'Gehen':gehen}
+dict_data = {'Datum':dates, 'Kommen':kommen, 'Pause':pause, 
+'Gehen':gehen, 'Soll_Zeit':soll_zeit, 'Ist_Zeit':ist_zeit, 'Zeitkonto':zeitkonto}
 
 df = pd.DataFrame(dict_data)
 df['Wochentag'] = wochentag
@@ -33,13 +41,57 @@ df['Datum'] = pd.to_datetime(df['Datum'])
 df = df[df['Wochentag'] != 'Samstag']
 df = df[df['Wochentag'] != 'Sonntag']
 
+df.to_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/full_data.csv', index=False)
+'''
+
 def refresh_and_save():
+   global df
+   df = pd.read_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/full_data.csv')
+   df['Datum'] = pd.to_datetime(df['Datum'])
+   df['Ist_Zeit'] = [timedelta_calc(df.Kommen[i], df.Gehen[i], df.Pause[i]) for i in range(len(df.Datum))]
+   df['Zeitkonto'] = [zeitkonto_calc(df.Ist_Zeit[i], df.Soll_Zeit[i]) for i in range(len(df.Datum))]
+   
    table_raw = df[df['Datum']>dt.datetime.strptime(zeiterfassung_entry.get(), "%Y-%m-%d")-dt.timedelta(days=5)]
    table_clean = table_raw[table_raw['Datum']<dt.datetime.strptime(zeiterfassung_entry.get(), "%Y-%m-%d")+dt.timedelta(days=5)]
-   zeiterfassung_entry_message.configure(
-      text=table_clean)
-   
+   table_clean.to_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/data.csv', index=False)
 
+   with open("C:/Users/paulg/Documents/Coding/Zeiterfassung/data.csv") as fp:
+      mytable = from_csv(fp)
+   zeiterfassung_entry_message.configure(
+      text=mytable)
+   if zeiterfassung_entry_times.get() != '00:00':
+      df.loc[df.Datum == zeiterfassung_entry.get(), combobox.get()] = zeiterfassung_entry_times.get()
+      zeiterfassung_entry_times.delete(0, 'end')
+      zeiterfassung_entry_times.insert(0, '00:00')
+      df.to_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/full_data.csv', index=False)
+      
+      table_raw = df[df['Datum']>dt.datetime.strptime(zeiterfassung_entry.get(), "%Y-%m-%d")-dt.timedelta(days=5)]
+      table_clean = table_raw[table_raw['Datum']<dt.datetime.strptime(zeiterfassung_entry.get(), "%Y-%m-%d")+dt.timedelta(days=5)]
+      table_clean.to_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/data.csv', index=False)
+      
+      with open("C:/Users/paulg/Documents/Coding/Zeiterfassung/data.csv") as fp:
+         mytable = from_csv(fp)
+   
+      zeiterfassung_entry_message.configure(
+         text=mytable)
+   df.to_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/full_data.csv', index=False)
+   df = pd.read_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/full_data.csv')
+   df['Datum'] = pd.to_datetime(df['Datum'])
+   df['Ist_Zeit'] = [timedelta_calc(df.Kommen[i], df.Gehen[i], df.Pause[i]) for i in range(len(df.Datum))]
+   df['Zeitkonto'] = [zeitkonto_calc(df.Ist_Zeit[i], df.Soll_Zeit[i]) for i in range(len(df.Datum))]
+   
+   table_raw = df[df['Datum']>dt.datetime.strptime(zeiterfassung_entry.get(), "%Y-%m-%d")-dt.timedelta(days=5)]
+   table_clean = table_raw[table_raw['Datum']<dt.datetime.strptime(zeiterfassung_entry.get(), "%Y-%m-%d")+dt.timedelta(days=5)]
+   table_clean.to_csv('C:/Users/paulg/Documents/Coding/Zeiterfassung/data.csv', index=False)
+
+   with open("C:/Users/paulg/Documents/Coding/Zeiterfassung/data.csv") as fp:
+      mytable = from_csv(fp)
+   zeiterfassung_entry_message.configure(
+      text=mytable)
+   zeiterfassung_statistik.configure(text=f'Zeitkontostand: {zeitkonto_insgesamt(df.Zeitkonto)}')
+   
+      
+      
 frame = customtkinter.CTkFrame(master=root,
                                width=1000,
                                height=500,
@@ -140,7 +192,18 @@ datum_combobox = customtkinter.CTkOptionMenu(master=root,
                                         dropdown_text_font=('Times New Roman', 24),
                                                            
                                         )
-'''                                        
+'''                    
+
+zeiterfassung_statistik = customtkinter.CTkLabel(root, 
+                              text='', 
+                              width=140,
+                              height=40,
+                              fg_color=("#0F3D3E", "white"),
+                              corner_radius=8, 
+                              text_font=('Times New Roman', 12),
+                              bg_color='black',
+                              text_color='white')
+                    
 #insert default text in Entry Boxes
 zeiterfassung_entry_times.insert(0, '00:00')
 zeiterfassung_entry.insert(0, dt.datetime.now().strftime("%Y-%m-%d"))
@@ -151,7 +214,7 @@ zeiterfassung_entry_message = customtkinter.CTkLabel(root,
                               height=40,
                               fg_color=("#0F3D3E", "white"),
                               corner_radius=8, 
-                              text_font=('Times New Roman', 10),
+                              text_font=('Times New Roman', 12),
                               bg_color='black',
                               text_color='white')
 
@@ -166,8 +229,9 @@ datum_label.grid(row=0, column=0, columnspan=2, rowspan=1, sticky='ew', padx=10)
 uhrzeit_label.grid(row=1, column=0, columnspan=2, rowspan=1, sticky='ew', padx=10)
 zeiterfassung_entry.grid(row=0, column=2, sticky='ew', padx=10, columnspan=4)
 zeiterfassung_message.grid(row=2, column=0, sticky='ew', padx=10, columnspan=6)
+zeiterfassung_statistik.grid(row=3, column=4, sticky='nsew', padx=10, columnspan=2, rowspan=6)
 zeiterfassung_entry_times.grid(row=1, column=4, padx=10, sticky='ew', columnspan=2)
-zeiterfassung_entry_message.grid(row=3, column=0, padx=10, sticky='nsew', columnspan=6, rowspan=6)
+zeiterfassung_entry_message.grid(row=3, column=0, padx=10, sticky='nsew', columnspan=4, rowspan=6)
 zeiterfassung_button.grid(row=9, column=0, columnspan=6, sticky='ew', padx=10)
 
 
--- main.py
+++ main.py
@@ -8,11 +8,118 @@ def daterange(date1, date2):
         yield date1 + dt.timedelta(n)
 
 
+def timedelta_calc(kommen, gehen, pause):
+    
+    #ALle Zeiten in Integer umrechnen
+    if kommen[0] == '0':
+        zeit_kommen_stunden = int(kommen[1])
+        zeit_kommen_minuten = int(kommen[3:5])
+    else:
+        zeit_kommen_stunden = int(kommen[0:2])
+        zeit_kommen_minuten = int(kommen[3:5])
+    
+    if gehen[0] == '0':
+        zeit_gehen_stunden = int(gehen[1])
+        zeit_gehen_minuten = int(gehen[3:5])
+    else:
+        zeit_gehen_stunden = int(gehen[0:2])
+        zeit_gehen_minuten = int(gehen[3:5]) 
+    
+    pause = int(pause[3:5])
+    
+    #Pause von der Gehen Zeit abziehen
+    if zeit_gehen_minuten - pause < 0:
+        zeit_gehen_minuten = 60 + zeit_gehen_minuten - pause
+        zeit_gehen_stunden -= 1
+    else:
+        zeit_gehen_minuten -= pause
+    
+    #Die Stunden ausrechnen wie lange ich da war
+    
+    zeit_stunden = zeit_gehen_stunden - zeit_kommen_stunden    
+    
+    #Die Minuten ausrechnen wie lang ich da war
+    
+    if zeit_gehen_minuten - zeit_kommen_minuten < 0:
+        zeit_minuten = 60 + zeit_gehen_minuten - zeit_kommen_minuten
+        zeit_stunden -= 1
+    else:
+        zeit_minuten = zeit_gehen_minuten - zeit_kommen_minuten
+    
+    if len(str(zeit_minuten)) == 1:
+        zeit_minuten = f'0{zeit_minuten}'
+                
+    if len(str(zeit_stunden)) == 1:
+        return f"0{zeit_stunden}:{zeit_minuten}" 
+    
+    return f"{zeit_stunden}:{zeit_minuten}"  
 
 
+def zeitkonto_calc(ist_zeit, soll_zeit):
 
-def write_to_csv():
-    pass
+    ist_zeit_stunden = int(ist_zeit[0:2])
+    ist_zeit_minuten = int(ist_zeit[3:5])
+    
+    soll_zeit_stunden = int(soll_zeit[0:2])
+    soll_zeit_minuten = int(soll_zeit[3:5])
+    
+    
+    if ist_zeit_stunden < soll_zeit_stunden:
+        if ist_zeit_minuten > soll_zeit_minuten:
+            zeit_stunden = f'0{abs(int(ist_zeit_stunden + 1 - soll_zeit_stunden))}'
+            zeit_minuten = f'{ist_zeit_minuten - soll_zeit_minuten}'
+                    
+            if len(zeit_minuten) == 1:
+                zeit_minuten = f'0{zeit_minuten}'
+            if int(zeit_stunden) == 0:
+                return f"{zeit_stunden}:{zeit_minuten}"
+            return f"-{zeit_stunden}:{zeit_minuten}"
+        else:    
+            zeit_stunden = f'{ist_zeit_stunden - soll_zeit_stunden}'
+            zeit_minuten = f'{ist_zeit_minuten - soll_zeit_minuten}'
+            if int(zeit_stunden) < 10:
+                zeit_stunden = f'0{abs(int(zeit_stunden))}'
+            if len(zeit_minuten) == 1:
+                zeit_minuten = f'0{zeit_minuten}'    
+            return f"-{zeit_stunden}:{zeit_minuten}"
+    else: 
+        zeit_stunden = f'{ist_zeit_stunden - soll_zeit_stunden}'
+        zeit_minuten = f'{ist_zeit_minuten - soll_zeit_minuten}'
+        if len(zeit_stunden) == 1:
+            zeit_stunden = f'0{zeit_stunden}'
+        if len(zeit_minuten) == 1:
+            zeit_minuten = f'0{zeit_minuten}' 
+        return f" {zeit_stunden}:{zeit_minuten}"
+
+    
+def zeitkonto_insgesamt(zeitkonto):
+    zeit = 0
+    for i in range(len(zeitkonto)):
+        #if zeitkonto[i] != '00:00':
+            #print(zeitkonto[i])
+        if zeitkonto[i][0] == ' ':
+            zeitkonto_std = int(zeitkonto[i][0:3])
+            zeitkonto_min = int(zeitkonto[i][4:6])
+       
+        elif zeitkonto[i][0] == '-':  
+            zeitkonto_std = int(zeitkonto[i][0:3])
+            zeitkonto_min = int(f'-{zeitkonto[i][4:6]}')
+            
+        else:
+            zeitkonto_std = int(zeitkonto[i][0:2])
+            zeitkonto_min = int(zeitkonto[i][3:5])          
+        if zeitkonto_min >= 0:
+            zeit += zeitkonto_std * 60 + zeitkonto_min
+        else: 
+            zeit -= (zeitkonto_min + 60 * zeitkonto_std)          
+    if zeit >= 60:
+        zeit_min = f'{zeit % 60}'
+        if len(zeit_min) == 1:
+            zeit_min = f'0{zeit_min}'
+        zeit_std = f' {(zeit - int(zeit_min)) // 60}'
+        if len(zeit_std) == 1:
+            zeit_std = f'0{zeit_std}'
+        return f' {zeit_std}:{zeit_min}'   
 
 
 dates = [dtt.strftime('%Y-%m-%d') for dtt in daterange(dt.datetime.strptime(f"{dt.datetime.now().year-2}-01-01", "%Y-%m-%d"),
